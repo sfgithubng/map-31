1f627351c8296359369773480f148bbe6f00e3ab
Sergei Fomin <xoma686@protonmail.com>
2025-10-05 18:05:29 +0100

    MAP-31: Fix 01/02/02.cpp sum calculation

--- 01/02/02.cpp
+++ 01/02/02.cpp
@@ -16,20 +16,17 @@ void populateVectorWithRandomData(std::vector<int32_t>& vectorToPopulate) {
     };
 }
 
-std::vector<int32_t> sumVectorsWorker(const std::vector<int32_t>& vectorA,
-                                    const std::vector<int32_t>& vectorB,
-                                    const int32_t& chunkStartIndex,
-                                    const int32_t& chunkEndIndex) {
-                                    std::vector<int32_t> resultingVector{};
-    resultingVector.resize(chunkEndIndex - chunkStartIndex);
+void sumVectorsWorker(const std::vector<int32_t>& vectorA,
+                   const std::vector<int32_t>& vectorB,
+                   std::vector<int32_t>& vectorResult,
+                   const int32_t& chunkStartIndex,
+                   const int32_t& chunkEndIndex) {
     for (int32_t elementIndex = chunkStartIndex; elementIndex <= chunkEndIndex; elementIndex++) {
-        resultingVector.push_back(vectorA.at(elementIndex) + vectorB.at(elementIndex));
+        vectorResult.at(elementIndex) = vectorA.at(elementIndex) + vectorB.at(elementIndex);
     }
-    return resultingVector;
 }
 
-std::chrono::duration<double> sumVectorsWithThreading(const std::vector<int32_t>& vectorA, const std::vector<int32_t>& vectorB, int32_t threadingFactor) {
-    const auto processingStartTime{std::chrono::steady_clock::now()};
+std::vector<int32_t> sumVectorsWithThreading(const std::vector<int32_t>& vectorA, const std::vector<int32_t>& vectorB, int32_t threadingFactor) {
     if (vectorA.size() != vectorB.size()) {
         throw std::invalid_argument("Vectors supplied to sum function aren't of the same length");
     }
@@ -37,23 +34,34 @@ std::chrono::duration<double> sumVectorsWithThreading(const std::vector<int32_t>
     int32_t chunkStartIndex = 0;
     int32_t chunkEndIndex = 0;
     std::vector<int32_t> vectorChunkStartIndexes;
+    std::vector<int32_t> vectorResult{};
+    vectorResult.resize(vectorA.size());
     std::vector<std::thread> workerThreads{};
     while (chunkStartIndex + vectorChunkSize < vectorA.size()) {
         vectorChunkStartIndexes.push_back(chunkStartIndex);
         chunkEndIndex = chunkStartIndex + vectorChunkSize;
-        workerThreads.push_back(std::thread(sumVectorsWorker, vectorA, vectorB, chunkStartIndex, chunkEndIndex));
+        workerThreads.push_back(std::thread(sumVectorsWorker,
+            std::cref(vectorA),
+            std::cref(vectorB),
+            std::ref(vectorResult),
+            chunkStartIndex,
+            chunkEndIndex)
+        );
         chunkStartIndex = chunkEndIndex + 1;
     }
-    workerThreads.push_back(std::thread(sumVectorsWorker, vectorA, vectorB, chunkStartIndex, vectorA.size() - 1));
+    workerThreads.push_back(std::thread(sumVectorsWorker,
+        std::cref(vectorA),
+        std::cref(vectorB),
+        std::ref(vectorResult),
+        chunkStartIndex, vectorA.size() - 1)
+    );
     for (auto& workerThread : workerThreads) {
         if (workerThread.joinable()) {
             workerThread.join();
         }
     }
-    const auto processingFinishTime{std::chrono::steady_clock::now()};
-    const std::chrono::duration<double> processingTime{processingFinishTime - processingStartTime};
 
-    return processingTime;
+    return vectorResult;
 }
 
 int32_t main() {
@@ -75,7 +83,10 @@ int32_t main() {
     for (const auto concurrencyFactor : concurrencyFactors) {
         std::cout << std::format(columnHeaderOutputFormat, "Thread count: " + std::to_string(concurrencyFactor));
         for (const auto vectorToSum : vectorsToSum) {
-            const auto processingTime = sumVectorsWithThreading(vectorToSum, vectorToSum, concurrencyFactor);
+            const auto processingStartTime{std::chrono::steady_clock::now()};
+            auto vectorsSum = sumVectorsWithThreading(vectorToSum, vectorToSum, concurrencyFactor);
+            const auto processingFinishTime{std::chrono::steady_clock::now()};
+            const std::chrono::duration<double> processingTime{processingFinishTime - processingStartTime};
             const auto processingTimeMilliSec = processingTime.count()*1000;
             const auto processingTimeForOutput = std::format(columnProcesingTimeOutputFormat, processingTimeMilliSec, "ms");
             std::cout << "|" << std::format(columnHeaderOutputFormat, processingTimeForOutput);
